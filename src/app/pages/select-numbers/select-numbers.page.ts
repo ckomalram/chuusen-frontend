963f471075ee5c5b0254a1f77a6c2a15b2668106
ckomalram <glaw14@gmail.com>
2022-11-28 20:33:06 -0500

    tab1 + ajustando nombres

--- src/app/pages/select-numbers/select-numbers.page.ts
+++ src/app/pages/select-numbers/select-numbers.page.ts
@@ -13,16 +13,57 @@ export class SelectNumbersPage implements OnInit {
   constructor() {}
 
   ngOnInit() {
-
-this.numbers = [
-  "00", "01", "02", "03", "04", "05", "06", "07", "08", "09", 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-  21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
-  40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58,
-  59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77,
-  78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96,
-  97, 98, 99,
-];
+// this.numbers = [
+//   0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+//   21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
+//   40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58,
+//   59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77,
+//   78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96,
+//   97, 98, 99,
+// ];
+this.initNumbers();
 
     console.log(this.numbers);
+  }
+
+  toogleNumber(dataSelected: any, index: number){
+    const number = this.numbers.find((value,index)=> value.number === dataSelected.number);
+
+    console.log(number);
+
+    if (number.selected) {
+      this.numbers[index].selected = false;
+    }else{
+      this.numbers[index].selected = true;
+    }
+  }
+
+  continuar(){
+    console.log(this.numbers);
+
+    const dataNumbers = this.numbers.filter(value =>value.selected === true);
+    const dataTicket = {
+      numbers: dataNumbers,
+      user: {},
+      sorteo: {}
+    }
+    console.log(dataTicket);
+  }
+
+  initNumbers(){
+    this.numbers = [];
+    for (let index = 0; index < 100; index++) {
+
+      this.numbers.push({
+        number: index,
+        selected: false,
+        // user: {},
+        count: 4
+      })
+    }
+  }
+
+  clearNumbersSelected(){
+    this.initNumbers();
   }
 }
